9bbe9a0fb44c6cbee7b81131f2737798c2002ca6
Dhuha Hasan <duha_9@yahoo.com>
2019-11-23 10:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
+
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payRatePerHour]) {
+    return obj = {
+        firstName: firstName
+        , familyName: familyName
+        , title: title
+        , payPerHour: payRatePerHour
+        , timeInEvents: []
+        , timeOutEvents: []
+    }
+}
+
+function createEmployees(arr) {
+    let arrayOfObj = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrayOfObj.push(createEmployeeRecord(arr[i]))
+    }
+    return arrayOfObj
+}
+
+function createTimeInEvent(obj, dateStamp) {
+    date = dateStamp.split(" ")
+    timeObj = {
+        type: "TimeIn"
+        , date: date[0]
+        , hour: date[1]
+    }
+    obj.timeInEvents.push(timeObj)
+    return obj
+}
+
+function createTimeOutEvent(obj, dateStamp) {
+    date = dateStamp.split(" ")
+    timeObj = {
+        type: "TimeOut"
+        , date: date[0]
+        , hour: date[1]
+    }
+    obj.timeOutEvents.push(timeObj)
+    return obj
+}
+
+function hoursWorkedOnDate(obj, date) {
+    for (let i = 0; i < obj.timeInEvents.length; i++) {
+        if (obj.timeInEvents[i].date == date && obj.timeOutEvents[i].date == date) {
+            return parseInt(obj.timeOutEvents[i].hour.slice(0,2)) - parseInt(obj.timeInEvents[i].hour.slice(0,2))
+        }
+    }
+}
+
+
+function wagesEarnedOnDate(obj, date) {
+    let hours=hoursWorkedOnDate(obj,date)
+    return hours*obj.payPerHour
+}
+
+function allWagesFor(obj) {
+    let allPayOwed=0
+    for(let i=0;i<obj.timeInEvents.length;i++){
+        allPayOwed+=wagesEarnedOnDate(obj,obj.timeInEvents[i].date)
+    }
+    return allPayOwed
+}
+
+function createEmployeeRecords(arrayOfArray){
+    let arrayOfObj=[]
+    for(let i=0;i<arrayOfArray.length;i++){
+       arrayOfObj.push(createEmployeeRecord(arrayOfArray[i]))
+    }
+    return arrayOfObj
+}
+
+ function findEmployeebyFirstName(srcArray,firstName){
+ 
+    for(let i=0;i<srcArray.length;i++){
+       if(srcArray[i].firstName===firstName){
+           return srcArray[i]
+       }
+    }
+ }
+
+ function calculatePayroll(arrayOfEmployee){
+    let allPayOwed=0
+    let payroll=0
+    for(let i=0;i<arrayOfEmployee.length;i++){
+        allPayOwed=0
+        for(let j=0;j<arrayOfEmployee[i].timeInEvents.length;j++){
+            allPayOwed+=wagesEarnedOnDate(arrayOfEmployee[i],arrayOfEmployee[i].timeInEvents[j].date)
+        }
+        allPayedToEmployee+=allPayOwed
+    }
+    return payroll
+ }
